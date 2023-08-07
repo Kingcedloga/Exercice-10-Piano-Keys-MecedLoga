42fc1b027dff90629f55bea5375fb0c21ac51c6e
Meced Loga <mecedloga@gmail.com>
2023-08-07 18:40:47 +0100

    add function PressEventHandler

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 // The keys and notes variables store the piano keys
-const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
+const keys = [ 'c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key' ];
 const notes = [];
 keys.forEach(function(key){
   notes.push(document.getElementById(key));
 })
 
 // Write named functions that change the color of the keys below
- keyPlay = (event)=> {
+ keyPlay = (event) => {
   event.target.style.backgroundColor ='red';
 }
-keyReturn = (event)=> {
+keyReturn = (event) => {
   event.target.style.backgroundColor =' ';
 }
   
 // Write a named function with event handler properties
 
-
+PressEventHandler = (note) => {
+  note.addEventListener('mousedown', keyPlay);
+  note.addEventListener('mouseup', keyReturn);
+}
 // Write a loop that runs the array elements through the function
 
 
